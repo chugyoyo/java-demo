53c120c438437b1961c559ac326d293ef7e734b0
吴泽欣 <wuzexin@dslyy.com>
2025-07-28 11:10:03 +0800

    静态代理、JDK 动态代理、Cglib 动态代理

---
+++ spring-web-yo/src/main/java/com/chugyoyo/web/proxy/StaticProxyTest.java
@@ -0,0 +1,25 @@
+package com.chugyoyo.web.proxy;
+
+import lombok.AllArgsConstructor;
+
+public class StaticProxyTest {
+    public static void main(String[] args) {
+        // 使用
+        UserService proxy = new UserServiceProxy(new UserServiceImpl());
+        proxy.save();
+    }
+}
+
+// 3. 静态代理类
+@AllArgsConstructor
+class UserServiceProxy implements UserService {
+
+    private UserService target;  // 持有真实对象
+
+    @Override
+    public void save() {
+        System.out.println("before method save()");
+        target.save();  // 调用真实对象方法
+        System.out.println("before method save()");
+    }
+}
---
+++ spring-web-yo/src/main/java/com/chugyoyo/web/proxy/UserService.java
@@ -0,0 +1,6 @@
+package com.chugyoyo.web.proxy;
+
+// 1. 抽象接口
+interface UserService {
+    void save();
+}
