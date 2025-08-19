866ad5170df4cdf25ebeda7ab361a86fde524494
chugyoyo <15815179079@163.com>
2025-08-19 15:39:35 +0800

    MVC、thymeleaf

---
+++ chug-web-yo/src/main/java/com/chugyoyo/web/mvc/TestMvcController.java
@@ -0,0 +1,28 @@
+package com.chugyoyo.web.mvc;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@RequestMapping(value = "test-mvc")
+@Controller
+public class TestMvcController {
+
+    @GetMapping("/testModelAndView")
+    public ModelAndView testModelAndView() {
+        // 写 new ModelAndView("/test-mvc-model") 会变成 /templates//test-mvc-model.html 找不到
+        ModelAndView mv = new ModelAndView("test-mvc-model");
+        mv.addObject("user", new UserDTO(1, "李四"));
+        return mv;
+    }
+}
+
+@Getter
+@AllArgsConstructor
+class UserDTO {
+    private Integer id;
+    private String name;
+}
